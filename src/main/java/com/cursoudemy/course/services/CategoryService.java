d63b90c572dcc8d718d5f5c4c6bb64c46218bab7
Daniel Santos <daniel.marinho@i2tech.com.br>
2021-07-18 00:35:26 -0300

    spring course

---
+++ src/main/java/com/cursoudemy/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.cursoudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursoudemy.course.entities.Category;
+import com.cursoudemy.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		
+		return obj.get();
+	}
+}
---
+++ src/main/java/com/cursoudemy/course/services/UserService.java
@@ -0,0 +1,62 @@
+package com.cursoudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.cursoudemy.course.entities.User;
+import com.cursoudemy.course.repositories.UserRepository;
+import com.cursoudemy.course.services.exceptions.DatabaseException;
+import com.cursoudemy.course.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			userRepository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public User update(Long id, User obj) {
+		try {
+			User entity = userRepository.getOne(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+}
